10b3a69aabe6fb0a0a3b5d190433389d679669a8
Frans Budi Kashira <fransbudikashira@gmail.com>
2024-06-13 14:57:16 +0700

    Menambahkan fitur Authentication

---
+++ app/src/main/java/com/fransbudikashira/chefies/data/remote/response/RegisterResponse.kt
@@ -0,0 +1,15 @@
+package com.fransbudikashira.chefies.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class RegisterResponse(
+
+    @field:SerializedName("error")
+    val error: Boolean,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("detail")
+    val detail: String
+)
